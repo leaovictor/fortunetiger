9d23b64dc360e26d6bfcf5ddc325cb195fed7d0a
Victor Leao <leaovic@gmail.com>
2023-10-10 09:43:09 -0300

    wip

--- script.js
+++ script.js
@@ -4,27 +4,34 @@ document.addEventListener("DOMContentLoaded", function () {
     const startButton = document.getElementById('start-button');
     const progressContainer = document.querySelector('.progress-container');
 
+    if (!progressBar || !progressText || !startButton || !progressContainer) {
+        console.error("Elementos não encontrados no DOM.");
+        return;
+    }
+
+    const REDIRECT_URL = 'https://www.radarbets.com/player-from-agent/agent/36i7s';
+    const TOTAL_STEPS = 100;
+    const PROGRESS_DURATION = 2000;
+
     startButton.addEventListener('click', function () {
         startButton.disabled = true;
         startButton.classList.add('button-hidden'); // Torna o botão invisível suavemente
-        setTimeout(function () {
-            progressContainer.style.top = '0'; // Move a barra para a posição original
-            progressContainer.style.opacity = '1'; // Torna a barra visível
-        }, 100); // Aguarda um breve atraso antes de exibir a barra
+
+        progressContainer.style.top = '0'; // Move a barra para a posição original
+        progressContainer.style.opacity = '1'; // Torna a barra visível
+
         simulateProgress();
     });
 
     function simulateProgress() {
         let progress = 0;
         const step = 1;
-        const totalSteps = 100;
-        const progressBarDuration = 2000; // 2 segundos
 
         function updateProgressBar() {
-            if (progress >= totalSteps) {
+            if (progress >= TOTAL_STEPS) {
                 progressText.textContent = `Completar [${progress}]`;
                 setTimeout(function () {
-                    window.location.href = 'https://www.radarbets.com/player-from-agent/agent/36i7s';
+                    window.location.href = REDIRECT_URL;
                 }, 500); // Redireciona após 0,5 segundos
                 return;
             }
@@ -32,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
             progress += step;
             progressBar.style.width = progress + '%';
 
-            if (progress < totalSteps) {
+            if (progress < TOTAL_STEPS) {
                 if (progress <= 20) {
                     progressText.textContent = `A iniciar informações [${progress}%]`;
                 } else if (progress <= 80) {
@@ -47,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         requestAnimationFrame(updateProgressBar);
         setTimeout(function () {
-            window.location.href = 'https://www.radarbets.com/player-from-agent/agent/36i7s';
-        }, progressBarDuration);
+            window.location.href = REDIRECT_URL;
+        }, PROGRESS_DURATION);
     }
 });
